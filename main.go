639a3b74775e99fdef39af3e6f9c0433dc17e526
andres <andresojedams@hotmail.com>
2021-08-10 16:23:22 -0500

    aa

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
+	"log"
 	"net/http"
+	"time"
 )
+
+type PageVariables struct {
+	Date string
+	Time string
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -14,7 +22,20 @@ func main() {
 	})
 
 	r.HandleFunc("/website/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Sadconf Platform website")
+		now := time.Now()              // find the time right now
+		HomePageVars := PageVariables{ //store the date and time in a struct
+			Date: now.Format("02-01-2006"),
+			Time: now.Format("15:04:05"),
+		}
+
+		t, err := template.ParseFiles("test.html") //parse the html file homepage.html
+		if err != nil {                            // if there is an error
+			log.Print("template parsing error: ", err) // log it
+		}
+		err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
+		if err != nil {                  // if there is an error
+			log.Print("template executing error: ", err) //log it
+		}
 	})
 
 	http.ListenAndServe(":9990", r)
