e034d386ce8dc34a4f9c4f9b67f71a21a4eaf043
andres <andresojedams@hotmail.com>
2021-08-10 16:35:31 -0500

    aa

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 			Time: now.Format("15:04:05"),
 		}
 
-		t, err := template.ParseFiles("test.html") //parse the html file homepage.html
-		if err != nil {                            // if there is an error
+		t, err := template.ParseFiles("./test.html") //parse the html file homepage.html
+		if err != nil {                              // if there is an error
 			log.Print("template parsing error: ", err) // log it
 		}
 		err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
