7135eb108a88ca462fda19a0923d14c6a4d9d607
andres <andresojedams@hotmail.com>
2021-08-10 16:16:42 -0500

    aa

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/gorilla/mux"
-	"html/template"
 	"net/http"
 )
 
@@ -16,29 +14,7 @@ func main() {
 	})
 
 	r.HandleFunc("/website/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("test.html")
-		if err != nil {
-			fmt.Println(err)
-		}
-		items := struct {
-			Country string
-			City    string
-		}{
-			Country: "Australia",
-			City:    "Paris",
-		}
-		//	t.Execute(w, items)
-		buf := &bytes.Buffer{}
-		err = t.Execute(buf, items)
-		if err != nil {
-			// Send back error message, for example:
-			http.Error(w, "Hey, Request was bad!", http.StatusBadRequest) // HTTP 400 status
-		} else {
-			// No error, send the content, HTTP 200 response status implied
-			buf.WriteTo(w)
-		}
-		//	name := r.URL.Path[len("/greet/"):]
-		//	fmt.Fprintf(w, "Hello %s\n", name)
+		fmt.Fprintf(w, "Sadconf Platform website")
 	})
 
 	http.ListenAndServe(":9990", r)
