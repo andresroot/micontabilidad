95492775a1e9033263ede95cf06ad1cc39f6d56c
andres <andresojedams@hotmail.com>
2021-08-12 14:55:49 -0500

    c

--- main.go
+++ main.go
@@ -15,8 +15,22 @@ type PageVariables struct {
 	Time string
 }
 
+func favicon(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s\n", r.RequestURI)
+	w.Header().Set("Content-Type", "image/x-icon")
+	w.Header().Set("Cache-Control", "public, max-age=7776000")
+	fmt.Fprintln(w, "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\n")
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
+		http.FileServer(http.Dir("static"))))
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Sadconf Platform 2021 555111")
@@ -25,12 +39,35 @@ func main() {
 	r.HandleFunc("/prueba/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Prueba")
 	})
-	r.HandleFunc("/website/", func(w http.ResponseWriter, r *http.Request) {
+
+	// default
+	r.HandleFunc("/default/", func(w http.ResponseWriter, r *http.Request) {
 
 		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
 			log.Fatal(err)
 		}
+		//fmt.Println(dir)
+
+		ts, err := template.ParseFiles(dir + "/vista/default.html")
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error1111"+err.Error()+dir, 500)
+			return
+		}
+
+		// We then use the Execute() method on the template set to write the template
+		// content as the response body. The last parameter to Execute() represents any
+		// dynamic data that we want to pass in, which for now we'll leave as nil.
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error222", 500)
+		}
+	})
+
+	r.HandleFunc("/website/", func(w http.ResponseWriter, r *http.Request) {
+
 		//fmt.Println(dir)
 
 		ts, err := template.ParseFiles(dir + "/vista/test.html")
