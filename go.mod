4304d5dd92ac90ed825641b6ba84f7084b5ebc9d
andres <andresojedams@hotmail.com>
2021-10-07 17:17:28 -0500

    asadsa

--- go.mod
+++ go.mod
@@ -1,6 +1,11 @@
 module github.com/andresroot/micontabilidad
+
 go 1.15
 
 require (
-	 github.com/gorilla/mux v1.8.0
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.2 // indirect
+	gorm.io/driver/postgres v1.1.2
+	gorm.io/gorm v1.21.15
 )
--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
-type PageVariables struct {
-	Date string
-	Time string
+var db *gorm.DB
+var err error
+
+type Tarea struct {
+	Id          uint
+	Titulo      string
+	Descripcion string
+	Estado      string
+}
+type Resultado struct {
+	Resultado bool
 }
 
 func favicon(w http.ResponseWriter, r *http.Request) {
@@ -22,7 +34,18 @@ func favicon(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\n")
 }
 
+var Raiz string
+
 func main() {
+
+	dsn := "host=192.168.1.7 user=postgres password=Murc4505 dbname=Base2020 port=5432 sslmode=disable "
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+
+		panic("failed to connect database")
+
+	}
 	r := mux.NewRouter()
 
 	Raiz, err := filepath.Abs(filepath.Dir(os.Args[0]))
@@ -39,54 +62,175 @@ func main() {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
 		http.FileServer(http.Dir(Raiz+"static/"))))
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Sadconf Platform 2021 555111")
-	})
-
-	r.HandleFunc("/prueba/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Prueba")
-	})
+	// default
+	//r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	ts, err := template.ParseFiles(Raiz + "vista/login.html")
+	//	if err != nil {
+	//		log.Println(err.Error())
+	//		http.Error(w, "Internal Server Error1111"+err.Error()+Raiz, 500)
+	//		return
+	//	}
+	//	err = ts.Execute(w, nil)
+	//	if err != nil {
+	//		log.Println(err.Error())
+	//		http.Error(w, "Internal Server Error222", 500)
+	//	}
+	//})
 
 	// default
-	r.HandleFunc("/default/", func(w http.ResponseWriter, r *http.Request) {
-
-		ts, err := template.ParseFiles(Raiz + "vista/default.html")
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var tareas []Tarea
+		//db.Order("age desc, name").Find(&users)
+		db.Order("Id").Find(&tareas)
+		for _, miFila := range tareas {
+			// bla bla
+			log.Println(miFila.Titulo)
+		}
+		ts, err := template.ParseFiles(Raiz + "vista/menu.html")
 		if err != nil {
 			log.Println(err.Error())
 			http.Error(w, "Internal Server Error1111"+err.Error()+Raiz, 500)
 			return
 		}
 
-		// We then use the Execute() method on the template set to write the template
-		// content as the response body. The last parameter to Execute() represents any
-		// dynamic data that we want to pass in, which for now we'll leave as nil.
-		err = ts.Execute(w, nil)
+		parametros := map[string]interface{}{
+			"tareas": tareas,
+		}
+
+		err = ts.Execute(w, parametros)
 		if err != nil {
 			log.Println(err.Error())
 			http.Error(w, "Internal Server Error222", 500)
 		}
 	})
 
-	r.HandleFunc("/website/", func(w http.ResponseWriter, r *http.Request) {
-
-		//fmt.Println(dir)
-
-		ts, err := template.ParseFiles(Raiz + "vista/test.html")
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "Internal Server Error1111"+err.Error()+Raiz, 500)
-			return
-		}
-
-		// We then use the Execute() method on the template set to write the template
-		// content as the response body. The last parameter to Execute() represents any
-		// dynamic data that we want to pass in, which for now we'll leave as nil.
-		err = ts.Execute(w, nil)
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "Internal Server Error222", 500)
-		}
-	})
+	r.Path("/Nuevatarea").HandlerFunc(Nuevatarea).Name("DocumentoNuevo")
+	r.Path("/Vertarea/{codigo}").HandlerFunc(Vertarea).Name("DocumentoNuevo")
+	r.Path("/Editartarea").HandlerFunc(Editartarea).Name("DocumentoNuevo")
 
 	http.ListenAndServe(":9990", r)
+}
+
+func Editartarea(w http.ResponseWriter, r *http.Request) {
+	var tempVenta Tarea
+	dsn := "host=192.168.1.7 user=postgres password=Murc4505 dbname=Base2020 port=5432 sslmode=disable "
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+
+		panic("failed to connect database")
+
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// carga informacion de la venta
+	err = json.Unmarshal(b, &tempVenta)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	user := Tarea{0, tempVenta.Titulo, tempVenta.Descripcion, tempVenta.Estado}
+	//var result bool
+	db.Model(&user).Where("Id= ?", tempVenta.Id).Update("Titulo", tempVenta.Titulo).Update("Descripcion", tempVenta.Descripcion).Update("Estado", tempVenta.Estado)
+	//db.Save(&user) // pass pointer of data to Create
+	//db.Model(&product).Update("Price", 2000)
+	var resultado bool
+	resultado = true
+
+	js, err := json.Marshal(Resultado{resultado})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+}
+
+func Nuevatarea(w http.ResponseWriter, r *http.Request) {
+	var tempVenta Tarea
+	dsn := "host=192.168.1.7 user=postgres password=Murc4505 dbname=Base2020 port=5432 sslmode=disable "
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+
+		panic("failed to connect database")
+
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// carga informacion de la venta
+	err = json.Unmarshal(b, &tempVenta)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	user := Tarea{0, tempVenta.Titulo, tempVenta.Descripcion, tempVenta.Estado}
+	//var result bool
+
+	result := db.Create(&user) // pass pointer of data to Create
+	if result.RowsAffected > 0 {
+		var resultado bool
+		resultado = true
+
+		js, err := json.Marshal(Resultado{resultado})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+
+	} else {
+		var resultado bool
+		resultado = false
+
+		js, err := json.Marshal(Resultado{resultado})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}
+
+}
+
+func Vertarea(w http.ResponseWriter, r *http.Request) {
+
+	dsn := "host=192.168.1.7 user=postgres password=Murc4505 dbname=Base2020 port=5432 sslmode=disable "
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+
+		panic("failed to connect database")
+
+	}
+
+	Codigo := mux.Vars(r)["codigo"]
+
+	var miTarea Tarea
+	db.Table("tareas").Where("Id = ?", Codigo).Scan(&miTarea)
+
+	js, err := json.Marshal(miTarea)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+	//result.RowsAffected
 }
